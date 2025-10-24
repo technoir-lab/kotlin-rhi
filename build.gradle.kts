ecc5c4be34e28af1448315e95d89af51a81576fd
technoir <technoir.dev@gmail.com>
2025-10-24 09:57:42 +0100

    Add deployment workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,13 @@ plugins {
 }
 
 dependencies {
+    dokka(project(":core"))
+    dokka(project(":backend:mock"))
+    dokka(project(":backend:vulkan"))
+    dokka(project(":backend:webgpu"))
+
     nmcpAggregation(project(":core"))
     nmcpAggregation(project(":backend:mock"))
     nmcpAggregation(project(":backend:vulkan"))
+    nmcpAggregation(project(":backend:webgpu"))
 }
