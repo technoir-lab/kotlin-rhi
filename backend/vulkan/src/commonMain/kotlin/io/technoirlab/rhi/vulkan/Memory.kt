a94dadd8bbdfd6f176785c2e96d9ad80e62e651a
technoir <technoir.dev@gmail.com>
2025-10-27 22:52:16 +0000

    Remove now default opt-in @ExperimentalForeignApi

--- backend/vulkan/src/commonMain/kotlin/io/technoirlab/rhi/vulkan/Memory.kt
+++ backend/vulkan/src/commonMain/kotlin/io/technoirlab/rhi/vulkan/Memory.kt
@@ -1,13 +1,11 @@
 package io.technoirlab.rhi.vulkan
 
 import kotlinx.cinterop.CArrayPointer
-import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.NativePlacement
 import kotlinx.cinterop.UIntVar
 import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.set
 
-@ExperimentalForeignApi
 internal fun NativePlacement.allocArrayOf(elements: List<UInt>): CArrayPointer<UIntVar> {
     val array = allocArray<UIntVar>(elements.size)
     var index = 0
